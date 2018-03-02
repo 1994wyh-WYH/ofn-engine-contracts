a57464f43e547e0c9ba85169a4fca04717aaebef
Ian Shipman <ian.shipman@cfxtrading.com>
2018-03-02 15:05:45 -0600

    adds ts declarations

---
+++ index.d.ts
@@ -0,0 +1,14 @@
+declare module "ofn-engine-contracts" {
+  export const ActivityLimiter : any;
+  export const AssetRegistry : any;
+  export const BalanceManager : any;
+  export const ContractProvider : any;
+  export const Document : any;
+  export const DoublyLinkedList : any;
+  export const Endorsement : any;
+  export const EntityRegistry : any;
+  export const Settlement : any;
+
+  export const summary : any;
+  export const addresses : any;
+}
