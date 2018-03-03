58881b0ff29bbd69d9d94c5fd827ec6216be457f
Ian Shipman <ian.shipman@cfxtrading.com>
2018-03-02 18:33:08 -0600

    consolidates type declarations

--- index.d.ts
+++ index.d.ts
@@ -9,6 +9,61 @@ declare module "ofn-engine-contracts" {
   export const EntityRegistry : any;
   export const Settlement : any;
 
-  export const summary : any;
+  export const summary : Summary;
   export const addresses : any;
+}
+
+// Type declarations 
+
+export interface Entity {
+  balances: AssociatedShares[];
+  delegates: string[];
+  email: string;
+  id: string;
+  name: string;
+  owner: string;
+  password: string;
+  phone: string;
+  privateKey: string;
+  tokens: {
+    ofn: string;
+  };
+}
+
+export interface AssociatedShares {
+  id: string;
+  shares: number;
+}
+
+export interface Asset {
+  id: string;
+  name: string;
+  shareNumber: number;
+  approvals: Identity[];
+  distribution: AssociatedShares[];
+}
+
+export interface Identity {
+  id: string;
+  owner: string;
+}
+
+export interface Transaction {
+  buyerIx: number;
+  sellerIx: number;
+  asset: string;
+  numberOfShares: number;
+  price: number;
+}
+
+export interface Summary {
+  controller: string;
+  accounts: Entity[];
+  assets: Asset[];
+  endorsements: {
+    entities: [number, number[]][];
+    assets: number[][];
+  };
+  transactions: Transaction[];
+  ofnToken: string;
 }
